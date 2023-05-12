d13f5d6e5accaf78a39359abe4d5585969f6dd66
Al3xand3r11 <alexander.nance11@gmail.com>
2023-05-11 23:05:45 -0700

    working api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react';
+import { atom, useRecoilState } from 'recoil';
+
+const moviesState = atom({
+  key: "movies",
+  default: [],
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [movies, setMovies] = useRecoilState(moviesState);
+  useEffect(() => {
+    const getMovies = async () => {
+      const url = "https://api.themoviedb.org/3/trending/all/day?api_key=02dbfb1ad973a3edfa76104ad041a875";
+      const resp = await fetch(url);
+      const body = await resp.json();
+      setMovies(body);
+    };
+
+    getMovies();
+  }, [setMovies]);
+
+return movies.results?.map((movie) => (
+  <div key={movie.url}>
+    <a href={movie.url}>
+      {movie.title}
+    </a>
+  </div>
+)); 
 }
 
 export default App;
